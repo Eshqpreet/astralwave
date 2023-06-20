b883694cf7cfee8c8d529da90e3df7710f00a999
Eshqpreet Singh <120357936+Eshqpreet@users.noreply.github.com>
2023-06-20 23:04:18 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,26 +1,44 @@
+import React, { useState } from "react";
 import { Box, CircularProgress } from "@mui/material";
-import React from "react";
 
 const UserImage = ({ image, size = "60px" }) => {
-  const timestamp = new Date().getTime(); // Generate a timestamp
-  const [isLoading, setIsLoading] = React.useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const handleImageLoad = () => {
     setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    setIsLoading(false);
+    setIsError(true);
   };
 
   return (
     <Box width={size} height={size}>
       {isLoading ? (
         <CircularProgress size={size} /> // Show a loading spinner
+      ) : isError ? (
+        // Handle error condition, e.g., display a default image or error message
+        <Box
+          width={size}
+          height={size}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="grey.200"
+        >
+          Error loading image
+        </Box>
       ) : (
         <img
           style={{ objectFit: "cover", borderRadius: "50%" }}
           width={size}
           height={size}
           alt="user"
-          src={`https://astralwave.onrender.com/assets/${image}?${timestamp}`}
+          src={`https://astralwave.onrender.com/assets/${image}`}
           onLoad={handleImageLoad} // Update the loading state when the image is loaded
+          onError={handleImageError} // Handle error when the image fails to load
         />
       )}
     </Box>
