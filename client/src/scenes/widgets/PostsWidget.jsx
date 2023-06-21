25bb905b58ae78427484cdd84cb96c9997865d4a
Eshqpreet Singh <120357936+Eshqpreet@users.noreply.github.com>
2023-06-21 09:06:00 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -1,42 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
+
+const PAGE_SIZE = 10; // Number of posts to fetch per page
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const getPosts = async () => {
-    //  http://localhost:3001
-    const response = await fetch("https://astralwave.onrender.com/posts", {
+  const fetchPosts = async (page) => {
+    const url = isProfile
+      ? `https://astralwave.onrender.com/posts/${userId}/posts?page=${page}&limit=${PAGE_SIZE}`
+      : `https://astralwave.onrender.com/posts?page=${page}&limit=${PAGE_SIZE}`;
+
+    const response = await fetch(url, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
-    dispatch(setPosts({ posts: data }));
-  };
 
-  const getUserPosts = async () => {
-    const response = await fetch(
-      `https://astralwave.onrender.com/posts/${userId}/posts`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
+    // If it's the first page, replace the existing posts with new data
+    // Otherwise, append the new posts to the existing ones
+    const updatedPosts = page === 1 ? data : [...posts, ...data];
+
+    dispatch(setPosts({ posts: updatedPosts }));
   };
 
   useEffect(() => {
-    if (isProfile) {
-      getUserPosts();
-    } else {
-      getPosts();
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    fetchPosts(currentPage);
+  }, [currentPage]); // Fetch posts whenever the current page changes
+
+  const handleLoadMore = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <>
@@ -67,6 +66,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
           />
         )
       )}
+
+      <button onClick={handleLoadMore}>Load More</button>
     </>
   );
 };
