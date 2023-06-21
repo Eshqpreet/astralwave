51cfd32b0d0537227a6a26569cb5d9bbd52e26a8
Eshqpreet Singh <120357936+Eshqpreet@users.noreply.github.com>
2023-06-21 09:24:10 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,73 +1,151 @@
-import { useEffect } from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+  SendOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, TextField, useTheme } from "@mui/material";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setPosts } from "state";
-import PostWidget from "./PostWidget";
+import { setPost } from "state";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
 
-const PostsWidget = ({ userId, isProfile = false }) => {
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments: initialComments,
+}) => {
+  const [isComments, setIsComments] = useState(false);
+  const [newComment, setNewComment] = useState("");
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
+  const [comments, setComments] = useState(initialComments);
 
-  const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
-  };
+  const { palette } = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.main;
 
-  const getUserPosts = async () => {
-    const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
+  const patchLike = async () => {
+    try {
+      const response = await fetch(`https://astralwave.onrender.com/posts/${postId}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update post like");
       }
-    );
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
+
+      const updatedPost = await response.json();
+      dispatch(setPost({ post: updatedPost }));
+    } catch (error) {
+      console.error("Error updating post like:", error);
+      // Handle the error if necessary
+    }
   };
 
-  useEffect(() => {
-    if (isProfile) {
-      getUserPosts();
-    } else {
-      getPosts();
+  const handleCommentChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  const handleCommentSubmit = async (event) => {
+    try {
+      const response = await fetch(`https://astralwave.onrender.com/posts/${postId}/comments`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: newComment }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create comment");
+      }
+
+      const createdComment = await response.json();
+      const updatedComments = [...comments, createdComment];
+      setComments(updatedComments);
+      setNewComment("");
+    } catch (error) {
+      console.error("Error creating comment:", error);
+      // Handle the error if necessary
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    event.preventDefault();
+  };
 
   return (
-    <>
-      {posts.map(
-        ({
-          _id,
-          userId,
-          firstName,
-          lastName,
-          description,
-          location,
-          picturePath,
-          userPicturePath,
-          likes,
-          comments,
-        }) => (
-          <PostWidget
-            key={_id}
-            postId={_id}
-            postUserId={userId}
-            name={`${firstName} ${lastName}`}
-            description={description}
-            location={location}
-            picturePath={picturePath}
-            userPicturePath={userPicturePath}
-            likes={likes}
-            comments={comments}
-          />
-        )
+    <WidgetWrapper m="2rem 0">
+      <Friend
+        friendId={postUserId}
+        name={name}
+        subtitle={location}
+        userPicturePath={userPicturePath}
+      />
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
+      {picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`https://astralwave.onrender.com/assets/${picturePath}`}
+        />
       )}
-    </>
+      <Box display="flex" alignItems="center" mt="0.25rem">
+        <IconButton onClick={patchLike}>
+          {isLiked ? <FavoriteOutlined sx={{ color: primary }} /> : <FavoriteBorderOutlined />}
+        </IconButton>
+        <Typography>{likeCount}</Typography>
+        <IconButton onClick={() => setIsComments(!isComments)}>
+          <ChatBubbleOutlineOutlined />
+        </IconButton>
+        <Typography>{comments.length}</Typography>
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
+      </Box>
+      {isComments && (
+        <Box mt="0.5rem">
+          {comments.map((comment) => (
+            <Box key={comment._id}>
+              <Divider />
+              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                {comment.content}
+              </Typography>
+            </Box>
+          ))}
+          <Divider />
+          <TextField
+            label="Write a comment"
+            value={newComment}
+            onChange={handleCommentChange}
+            fullWidth
+          />
+          <IconButton type="submit" onClick={handleCommentSubmit}>
+            <SendOutlined />
+          </IconButton>
+        </Box>
+      )}
+    </WidgetWrapper>
   );
 };
 
-export default PostsWidget;
+export default PostWidget;
